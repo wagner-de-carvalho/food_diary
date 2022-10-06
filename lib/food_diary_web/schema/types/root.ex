1bc776298ca3dc16b28e7b49b69061fee17af73b
wagner <wagncarv@outlook.com>
2022-10-06 18:48:51 -0300

    Configuração inicial do projeto

---
+++ lib/food_diary_web/schema/types/root.ex
@@ -0,0 +1,14 @@
+defmodule FoodDiaryWeb.Schema.Types.Root do
+  use Absinthe.Schema.Notation
+  alias FoodDiaryWeb.Resolvers.User, as: UsersResolver
+
+  import_types FoodDiaryWeb.Schema.Types.User
+
+  object :root_query do
+    field :user, type: :user do
+      arg :id, non_null(:id)
+
+      resolve &UsersResolver.get/2
+    end
+  end
+end
