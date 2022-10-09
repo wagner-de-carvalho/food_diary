beffcef43cf7d8f8b8d1accc1d74267246cd4953
wagner <wagncarv@outlook.com>
2022-10-09 20:51:27 -0300

    Descrição de queries e tipos

--- lib/food_diary_web/schema/types/user.ex
+++ lib/food_diary_web/schema/types/user.ex
@@ -1,9 +1,10 @@
 defmodule FoodDiaryWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
+  @desc "Logic user representation"
   object :user do
-    field :id, non_null(:id)
-    field :name, non_null(:string)
-    field :email, non_null(:string)
+    field :id, non_null(:id), description: "User's id. Must be an integer."
+    field :name, non_null(:string), description: "User's name. Must be a string."
+    field :email, non_null(:string), description: "User's e-mail. Must be a string."
   end
 end
