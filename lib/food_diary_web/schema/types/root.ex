55da85edf9b2b9e8ba11f12256d83b606253f636
wagner <wagncarv@outlook.com>
2022-10-09 23:26:43 -0300

    Deletando com mutation

--- lib/food_diary_web/schema/types/root.ex
+++ lib/food_diary_web/schema/types/root.ex
@@ -19,5 +19,12 @@ defmodule FoodDiaryWeb.Schema.Types.Root do
 
       resolve &UsersResolver.create/2
     end
+
+    @desc "Deletes an user"
+    field :delete_user, type: :user do
+      arg :id, non_null(:id)
+
+      resolve &UsersResolver.delete/2
+    end
   end
 end
