9f1755b024feac4ed8a95efea8c4faadf57b43e0
wagner <wagncarv@outlook.com>
2022-10-10 00:07:21 -0300

    Listas de tipos enum

--- lib/food_diary_web/schema/types/user.ex
+++ lib/food_diary_web/schema/types/user.ex
@@ -6,6 +6,7 @@ defmodule FoodDiaryWeb.Schema.Types.User do
     field :id, non_null(:id), description: "User's id. Must be an integer."
     field :name, non_null(:string), description: "User's name. Must be a string."
     field :email, non_null(:string), description: "User's e-mail. Must be a string."
+    field :meals, list_of(:meal)
   end
 
   input_object :create_user_input do
